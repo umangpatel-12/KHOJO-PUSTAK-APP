b84386e2dd4dd502292ad45822c0546342338ff9
Patel Umangkumar <umangkumar170@gmail.com>
2025-09-29 11:59:05 +0530

    Update Profile Page

--- lib/Widgets/Screens/ProfileScreen.dart
+++ lib/Widgets/Screens/ProfileScreen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+
+import 'Widgets/ProfileCard.dart';
 
 class Profilescreen extends StatefulWidget {
   const Profilescreen({super.key});
@@ -10,6 +14,252 @@ class Profilescreen extends StatefulWidget {
 class _ProfilescreenState extends State<Profilescreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      // backgroundColor: Colors.white60,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        shadowColor: Colors.black12,
+        elevation: 2,
+        title: Text(
+          "Profile",
+          style: TextStyle(
+            fontSize: 22,
+            color: Colors.green,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(left: 16,right: 16,bottom: 16),
+        child: SizedBox(
+          child: Column(
+            children: [
+
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 35,
+                    backgroundColor: Colors.black26,
+                    child: Image.asset(
+                      "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(width: 12),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text("Rahul Sharma", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                      SizedBox(height: 4),
+                      // Row(
+                      //   children: [
+                      //     Icon(Icons.star, color: Colors.amber, size: 18),
+                      //     SizedBox(width: 4),
+                      //     Text("4.8 (127 reviews)", style: TextStyle(fontSize: 14, color: Colors.black54)),
+                      //   ],
+                      // ),
+                      SizedBox(height: 4),
+                      Text("Mumbai, Maharashtra", style: TextStyle(fontSize: 14, color: Colors.black54)),
+                    ],
+                  ),
+                  const Spacer(),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                        "Edit",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+
+              const SizedBox(height: 16),
+
+              SizedBox(height: 16,),
+
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.06),
+                      blurRadius: 18,
+                      offset: const Offset(0, 8),
+                    )
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(0.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(height: 8,),
+                      Text(
+                        "     My Activity",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black45
+                        ),
+                      ),
+
+                      ListView(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        children: [
+                          ActivityTile(
+                            icon: Icons.shopping_bag_outlined,
+                            title: "My Orders",
+                            subtitle: "Track your purchases",
+                            count: 3,
+                            onTap: () {
+                              // Action for My Orders
+                              print("My Orders tapped");
+                            },
+                          ),
+                          ActivityTile(
+                            icon: Icons.inventory_2_outlined,
+                            title: "My Listings",
+                            subtitle: "Manage your book listings",
+                            count: 8,
+                            onTap: () {
+                              print("My Listings tapped");
+                            },
+                          ),
+                          ActivityTile(
+                            icon: Icons.favorite_border,
+                            title: "Wishlist",
+                            subtitle: "Saved books",
+                            count: 12,
+                            onTap: () {
+                              print("Wishlist tapped");
+                            },
+                          ),
+
+                          ActivityTile(
+                            icon: Icons.history,
+                            title: "Transaction History",
+                            subtitle: "All your transactions",
+                            onTap: () {
+                              print("Transaction History tapped");
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 16,),
+
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.06),
+                      blurRadius: 18,
+                      offset: const Offset(0, 8),
+                    )
+                  ],
+                ),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(height: 8,),
+                      Text(
+                        "     Account",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black45
+                        ),
+                      ),
+
+                      ListView(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        children: [
+                          ActivityTile(
+                            icon: CupertinoIcons.person,
+                            title: "Personal Information",
+                            subtitle: "Update your details",
+                            onTap: () {
+                              // Action for My Orders
+                              print("Personal Information tapped");
+                            },
+                          ),
+                          ActivityTile(
+                            icon: CupertinoIcons.location,
+                            title: "Location",
+                            subtitle: "Updates your location's",
+                            onTap: () {
+                              print("Location tapped");
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+
+              SizedBox(height: 16,),
+
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.06),
+                      blurRadius: 18,
+                      offset: const Offset(0, 8),
+                    )
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    ListView(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      children: [
+                        ActivityTile(
+                          icon: Iconsax.logout_1,
+                          color: Colors.red,
+                          title: "Logout",
+                          subtitle: "Sign out of your account",
+                          onTap: () {
+                            // Action for My Orders
+                            print("Personal Information tapped");
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 80,)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/Widgets/Screens/Widgets/ProfileCard.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class ActivityTile extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+  final int? count;
+  final color;
+  final VoidCallback? onTap;
+
+  const ActivityTile({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+    this.count,
+    this.onTap,
+    this.color
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent, // background transparent
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(10),
+        splashColor: Colors.black.withOpacity(0.2), // ripple color
+        highlightColor: Colors.green.withOpacity(0.1), // pressed color
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 6),
+          child: ListTile(
+            leading: Icon(icon, color: color),
+            title: Text(
+              title,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+                color: color,
+              ),
+            ),
+            subtitle: Text(
+                subtitle,
+              style: TextStyle(
+                fontSize: 13
+              ),
+            ),
+            trailing: count != null
+                ? CircleAvatar(
+              radius: 12,
+              backgroundColor: Colors.green.shade50,
+              child: Text(
+                count.toString(),
+                style: const TextStyle(
+                  color: Colors.green,
+                  fontSize: 12
+                ),
+              ),
+            )
+                : null,
+          ),
+        ),
+      ),
+    );
+  }
+}
